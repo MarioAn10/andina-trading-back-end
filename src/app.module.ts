674cbaac715d1fcc19e0a2cd2897ab3dbd9a85c9
Mario Andrés Martínez García <mario.martinezg96@gmail.com>
2024-05-28 16:49:15 -0500

    Finalización de lógica de la consulta de imágenes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { join } from 'path';
+
 import { CommonModule } from './common/common.module';
 import { ProductsModule } from './products/products.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 
 @Module({
@@ -20,6 +23,9 @@ import { FilesModule } from './files/files.module';
             password: process.env.DB_PASSWORD,
             autoLoadEntities: true,
             synchronize: true,
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public')
         }),
         CommonModule,
         ProductsModule,
